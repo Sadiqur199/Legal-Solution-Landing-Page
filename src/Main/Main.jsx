5d3cf7d81953577f0756fdc1a55d926f97c69766
Sadiqur199 <sadiqurrhaman199@gmail.com>
2024-06-02 01:26:48 +0600

    footer section implement

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import Navbar from '../Component/Navbar/Navbar';
-import Banner from '../Component/Banner/Banner';
-import PracticeArea from '../Component/PracticeArea/PracticeArea';
-import Testimonials from '../Component/Testimonials/Testimonials';
-import FAQ from '../Component/FAQ/FAQ';
-import Contact from '../Component/Contact/Contact';
+import React from "react";
+import Navbar from "../Component/Navbar/Navbar";
+import Banner from "../Component/Banner/Banner";
+import PracticeArea from "../Component/PracticeArea/PracticeArea";
+import Testimonials from "../Component/Testimonials/Testimonials";
+import FAQ from "../Component/FAQ/FAQ";
+import Contact from "../Component/Contact/Contact";
+import Footer from "../Component/Footer/Footer";
 
 const Main = () => {
   return (
-    <div className='container md:px-28 sm:px-0'>
-      <Navbar></Navbar>
-      <Banner></Banner>
-      <PracticeArea></PracticeArea>
-      <Testimonials></Testimonials>
-      <FAQ></FAQ>
-      <Contact></Contact>
+    <div>
+      <div className="container md:px-28 sm:px-0">
+        <Navbar></Navbar>
+        <Banner></Banner>
+        <PracticeArea></PracticeArea>
+        <Testimonials></Testimonials>
+        <FAQ></FAQ>
+        <Contact></Contact>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
